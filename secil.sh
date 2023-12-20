a7f2ebee8046bd318aa5841aaecbd6127ba88551
Secil <secilnur@outlook.com.tr>
2023-12-20 23:26:10 +0300

    image

--- secil.sh
+++ secil.sh
@@ -1,3 +1,5 @@
 #brew install kind
-kind create cluster --config config.yaml --image=kindest/node:v1.21.1 --name secil
+#kind create cluster --config config.yaml --image=kindest/node:v1.21.1 --name secil
+kind create cluster --config config.yaml --image=kindest/node:v1.23.0 --name secil
+#son version 1.29.0
 #kind delete cluster  --name secil
